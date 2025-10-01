1d090e6281cb9521c9f97ac06a304cf634fba02b
Winston <leonardwinstoncx9@gmail.com>
2025-10-02 00:10:34 +0800

    Initial unit tests for cart.js
    - Initial unit tests for cart.js
    - Fixed a bug regarding duplicated items in the CartPage

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -21,15 +21,17 @@ export default {
 
   // only run these tests
   testMatch: [
-    "<rootDir>/client/src/pages/Auth/*.test.js",
-    "<rootDir>/client/src/pages/*.test.js",
+    // "<rootDir>/client/src/pages/Auth/*.test.js",
+    // "<rootDir>/client/src/pages/*.test.js",
+    "<rootDir>/client/src/context/cart.test.js",
   ],
 
   // jest code coverage
   collectCoverage: true,
   collectCoverageFrom: [
-    "client/src/pages/Auth/**",
-    "client/src/pages/HomePage.js",
+    // "client/src/pages/Auth/**",
+    // "client/src/pages/HomePage.js",
+    "client/src/context/cart.js",
   ],
   coverageThreshold: {
     global: {
