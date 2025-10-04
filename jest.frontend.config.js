282ab2196a005fb15b72d46467209797a0621ab5
TabrizPlv <tabrizpahlavi@gmail.com>
2025-10-04 16:20:27 +0800

    update jest configs

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -21,21 +21,19 @@ export default {
 
   // only run these tests
   testMatch: [
-    "<rootDir>/client/src/pages/Auth/*.test.js",
-    "<rootDir>/client/src/pages/*.test.js",
-    "<rootDir>/client/src/context/cart.test.js",
-    "<rootDir>/client/src/hooks/useCategory.test.js",
+    "<rootDir>/client/src/pages/**/*.test.js",
+    "<rootDir>/client/src/context/**/*.test.js",
+    "<rootDir>/client/src/hooks/**/*.test.js",
+    "<rootDir>/client/src/components/**/*.test.js",
   ],
 
   // jest code coverage
   collectCoverage: true,
   collectCoverageFrom: [
-    "client/src/pages/Auth/**",
-    "client/src/pages/HomePage.js",
-    "client/src/context/cart.js",
-    "client/src/pages/CartPage.js",
-    "client/src/hooks/useCategory.js",
-    "client/src/pages/Categories.js",
+    "client/src/pages/*/**",
+    "client/src/context/*/**",
+    "client/src/hooks/*/**",
+    "client/src/components/*/**",
   ],
   coverageThreshold: {
     global: {
